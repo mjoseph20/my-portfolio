dcc4578721b601e7f8abd7c03864680d9431b2ff
mjoseph2013 <mjoseph20@student.gsu.edu>
2025-05-07 16:13:49 -0400

    optimized contact form for mobile

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -36,18 +36,18 @@ export const Contact = () => {
     >
       <RevealOnScroll>
         <div className="px-4 w-150">
-          <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-purple-500 to-orange-400 bg-clip-text text-transparent text-center">
+          <h2 className="text-2xl md:text-3xl font-bold mb-8 bg-gradient-to-r from-purple-500 to-orange-400 bg-clip-text text-transparent text-center">
             Let's Build Together
           </h2>
           <form className="space-y-6" onSubmit={handleSubmit}>
-            <div className="relative">
+            <div className="relative flex justify-center">
               <input
                 type="text"
                 id="name"
                 name="name"
                 required
                 value={formData.name}
-                className="w-full bg-white/5 border border-white rounded px-4 py-3 text-white transition focus:outline-none focus:border-blue-500 focus:bg-blue-500/5"
+                className="w-1/3 md:w-full bg-white/5 border border-white rounded px-4 py-3 text-white transition focus:outline-none focus:border-blue-500 focus:bg-blue-500/5"
                 placeholder="name"
                 onChange={(e) =>
                   setFormData({ ...formData, name: e.target.value })
@@ -55,14 +55,14 @@ export const Contact = () => {
               />
             </div>
 
-            <div className="relative">
+            <div className="relative flex justify-center">
               <input
                 type="email"
                 id="email"
                 name="email"
                 required
                 value={formData.email}
-                className="w-full bg-white/5 border border-white rounded px-4 py-3 text-white transition focus:outline-none focus:border-blue-500 focus:bg-blue-500/5"
+                className="w-1/3 md:w-full bg-white/5 border border-white rounded px-4 py-3 text-white transition focus:outline-none focus:border-blue-500 focus:bg-blue-500/5"
                 placeholder="example@mail.com"
                 onChange={(e) =>
                   setFormData({ ...formData, email: e.target.value })
@@ -70,26 +70,28 @@ export const Contact = () => {
               />
             </div>
 
-            <div className="relative">
+            <div className="relative flex justify-center">
               <textarea
                 id="messsage"
                 name="messsage"
                 required
                 rows={5}
                 value={formData.message}
-                className="w-full bg-white/5 border border-white rounded px-4 py-3 text-white transition focus:outline-none focus:border-blue-500 focus:bg-blue-500/5"
+                className="w-1/3 md:w-full bg-white/5 border border-white rounded px-4 py-3 text-white transition focus:outline-none focus:border-blue-500 focus:bg-blue-500/5"
                 placeholder="drop your message here — i’ll get back to you soon...."
                 onChange={(e) =>
                   setFormData({ ...formData, message: e.target.value })
                 }
               />
             </div>
-            <button
-              type="submit"
-              className="w-full bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.4)]"
-            >
-              Send Message
-            </button>
+            <div className="relative flex justify-center">
+              <button
+                type="submit"
+                className="w-1/3 md:w-full bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.4)]"
+              >
+                Send Message
+              </button>
+            </div>
           </form>
         </div>
       </RevealOnScroll>
